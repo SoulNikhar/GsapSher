797326aedde8353a50b61a709fc8d2b7f21c84d6
SoulNikhar <111144948+SoulNikhar@users.noreply.github.com>
2023-08-26 11:44:23 +0530

    Update script.js
    Type writer effect js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
+// ----------------------- This is for smooth scroll effect-----------------------
 const scroll = new LocomotiveScroll({
     el:document.querySelector('.main'),
     smooth:true
 });
 
+
+// ----------------------- This is for special effect -----------------------
 var t1 = gsap.timeline()
 
 t1.to(".main",{
@@ -23,3 +26,45 @@ t1.to(".main",{
     scale:1,
     duration:0.8
 })
+
+
+// ----------------------- This is for typewriter Effect---------------------------------
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const phrases = ["Web Developer", "UI/UX designer", "java coder",];
+  const el = document.getElementById("typewriter");
+
+  let sleepTime = 100;
+
+  let curPhraseIndex = 0;
+
+  const writeLoop = async () => {
+    while (true) {
+      let curWord = phrases[curPhraseIndex];
+
+      for (let i = 0; i < curWord.length; i++) {
+        el.innerText = curWord.substring(0, i + 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 10);
+
+      for (let i = curWord.length; i > 0; i--) {
+        el.innerText = curWord.substring(0, i - 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 5);
+
+      if (curPhraseIndex === phrases.length - 1) {
+        curPhraseIndex = 0;
+      } else {
+        curPhraseIndex++;
+      }
+    }
+  };
+
+  writeLoop();
